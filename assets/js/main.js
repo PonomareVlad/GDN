ed1e15b105799042541eec2dab6bc11705929961
PonomareVlad <sys.system@mail.ru>
2018-11-12 05:03:24 +0500

    Side menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,7 @@ function mainPage() {
     }
     document.body.innerHTML = `
 <header class="navigation-menu">
-    <button class="menu-button"></button>
+    <button class="menu-button" onclick="document.querySelector('.side-menu').classList.toggle('active',true);"></button>
     <button class="search-button"></button>
 </header>
 <main>
@@ -26,4 +26,43 @@ function mainPage() {
     <button class="podcasts">Podcasts</button>
     <button class="profile">Profile</button>
 </footer>`;
+    genSideMenu();
+}
+
+function genSideMenu() {
+    let menuWrapper = document.createElement('aside');
+    menuWrapper.classList.add('side-menu');
+    menuWrapper.innerHTML = `
+<div class="menu-header">
+    <button class="close-menu" onclick="document.querySelector('.side-menu').classList.toggle('active',false);">Close</button>
+    <span>Menu</span>
+    <button class="edit-menu">Edit</button>
+</div>
+<div class="menu-body">
+    <div class="menu-section">
+        <div class="title">Shortcuts</div>
+        <div class="menu-item">Favorites</div>
+        <div class="menu-item">Downloads</div>
+        <div class="menu-item danger-item">Log Out</div>
+    </div>
+    <div class="menu-section">
+        <div class="title">Browse by Feed</div>
+        <div class="menu-item">SDR</div>
+        <div class="menu-item">Subway Creatures</div>
+        <div class="menu-item">Alexander Ljung</div>
+        <div class="menu-item">Trashae Hubbard</div>
+        <div class="menu-item">Benedikt Safiyulin</div>
+        <div class="menu-item">Yogarasa Gandhi</div>
+        <div class="menu-item">Maria Illescas</div>
+        <div class="menu-item">Lacara Jones</div>
+        <div class="menu-item">Gabie Sheber</div>
+        <div class="menu-item">Ahmad Nazeri</div>
+        <div class="menu-item">Xun Guiying</div>
+        <div class="menu-item">Emiliya Perevalova</div>
+        <div class="menu-item">Sukhbirpal Dhalan</div>
+        <div class="menu-item">Hu Hyon-Suk</div>
+        <div class="menu-item">Zikoranaudodimma Nkechi</div>
+    </div>
+</div>`;
+    document.body.insertBefore(menuWrapper, document.body.querySelector('main'));
 }
